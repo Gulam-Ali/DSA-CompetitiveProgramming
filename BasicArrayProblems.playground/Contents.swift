676a28c8791207ea6a6869da6787cbc993791c58
Gulam Ali <gulam@signitysolutions.com>
2024-05-10 09:58:54 +0530

    problems on arrays

--- BasicArrayProblems.playground/Contents.swift
+++ BasicArrayProblems.playground/Contents.swift
@@ -38,22 +38,79 @@ import UIKit
 
 func hasPair(A:[Int], B:Int) -> Bool{
     if A.count > 0 {
-        for i in A.enumerated(){
-            for (i.offset + 1)..<A.count{
-                
+        for i in 0..<A.count{
+            for j in (i+1)..<A.count{
+                if A[i] + A[j] == B{
+                    return true
+                }
             }
         }
     }
     return false
 }
 
-print(hasPair(A: [2,5,6], B: 8))
+//print(hasPair(A: [2,5,6], B: 12))
+
+/*
+ Given an array of size N, Reverse entire array such as SC = O(1) (without taking extra steps)
+ */
+
+func reverseArr(A: inout [Int]) -> [Int]{
+    var i = 0
+    var j = A.count - 1
+    while (i <= j){
+        let temp = A[i]
+         A[i] = A[j]
+        A[j] = temp
+        i += 1
+        j -= 1
+    }
+    return A
+}
+
+//var input = [-1,7,8,2,4,0,4]
+//print(reverseArr(A: &input))
+
+/*
+ Given an array of size N and Si(start index) and Ei(end index), reverse the array from Si to Ei.
+ */
+
+func reversArrByIndices(A: inout [Int], Si: Int, Ei:Int) -> [Int]{
+    var i = Si
+    var j = Ei
+    
+    while (i < j){
+        let temp = A[i]
+        A[i] = A[j]
+        A[j] = temp
+        i += 1
+        j -= 1
+    }
+    return A
+}
+
+//var input = [-1,7,8,2,4,0,4]
+//print(reversArrByIndices(A: &input, Si: 3, Ei: 6))
 
 
 
+/*
+ Given an array A, rotate the array by K times from last to first (Amazon,,Google)
+ OR
+ Given an integer array A of size N and an integer B, you have to return the same array after rotating it B times towards the right.
+ ex - [1,2,3,4,5,6,7,8] when K = 2
+Ans - [8,1,2,3,4,5,6,7] > [7,8,1,2,3,4,5,6]
+ */
 
+func rotateArr(A: inout [Int], B:Int) -> [Int]{
+    A = reversArrByIndices(A: &A, Si: 0, Ei: A.count - 1)
+    A = reversArrByIndices(A: &A, Si: 0, Ei: B - 1)
+    A = reversArrByIndices(A: &A, Si: B, Ei: A.count - 1)
+    return A
+}
 
-
+var input = [1, 2, 3, 4]
+print(rotateArr(A: &input, B: 2))
 
 
 
